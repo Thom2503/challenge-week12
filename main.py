420ce4ebdb357146ab2cf81e5c63aa5fb5fde274
Robin Otterspeer <robin.otterspeer2005@gmail.com>
2022-11-30 11:59:05 +0100

    menu en docstrings

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from prettytable import PrettyTable
 from urllib.request import urlopen
-from Investor import Investor
 import json
 import statistics
 
 
 def parse_json() -> dict:
     """
+    haald data via de url en stopt deze in een dict.
 
+    :return coins_data: een dict met coin als key en waarde als value.
     """
     coins_data = {}
 
@@ -19,14 +20,17 @@ def parse_json() -> dict:
         'ELG',
         'FAW'
     ]
+    # coins: list met alle coins.
 
     for coin in coins:
         url = f"https://api.basecampcrypto.nl/v1/coin/{coin}/history?key=gxV34Ebr5s6ul15q"
+        # url: de url van de server met de data.
         url = urlopen(url).read()
 
         data = json.loads(url)
 
         history_data = []
+        # history_data: list met de waarde die de coin toen was.
         for history in data['history']:
             history_data.append(float(history['value']))
 
@@ -36,7 +40,13 @@ def parse_json() -> dict:
 
 
 def calc_days(coin_data: list, func: callable) -> int:
+    """
+    berkent het aantal dagen waarin de waarde hoger/lager is dan de dag ervoor.
 
+    :param coin_data: list met data van de coins
+
+    :return days: het aantal dagen dat hoger/lager zijn
+    """
     days = 0
     previous_value = 0
 
@@ -50,7 +60,14 @@ def calc_days(coin_data: list, func: callable) -> int:
 
 
 def calc_ldays(coin_data: list, func: callable) -> int:
+    """
+    berkent de streak waarin het aantal dagen waarin de
+    waarde hoger/lager is dan de dag ervoor.
 
+    :param coin_data: list met data van de coins
+
+    :return max(lst_days): de hoogste value uit de list is de hoogste streak
+    """
     days = 0
     lst_days = []
     previous_value = 0
@@ -67,10 +84,15 @@ def calc_ldays(coin_data: list, func: callable) -> int:
 
 
 def draw_table(coins_data: dict) -> None:
-    """"""
+    """
+    maakt een table met pretty tables met de berekeningen die je maakt.
+
+    :param coin_data: dict met data van de coins
+    """
     table = PrettyTable()
 
-    table.field_names = ['₿', 'AVG', 'MIN', 'MAX', 'SD', 'Q1', ' Q2', 'Q3', 'RNG', 'IQR', 'UPS', 'DOWNS', 'LUP', 'LDWN']
+    table.field_names = ['₿', 'AVG', 'MIN', 'MAX', 'SD', 'Q1',
+                         ' Q2', 'Q3', 'RNG', 'IQR', 'UPS', 'DOWNS', 'LUP', 'LDWN']
 
     for coin, history in coins_data.items():
         row_data = []
@@ -102,11 +124,39 @@ def draw_table(coins_data: dict) -> None:
     print(table)
 
 
-if __name__ == "__main__":
-    start_money = 1_000_000
-    coins_data = parse_json()
-    draw_table(coins_data)
+def main():
+    """
+    De main menu hier kan je een keuze maken voor welke data je wil zien.
+    Voor de table met de waarde van de coins druk je op T.
+    Voor de uitslag van de investeres druk op I
+    Voor de grafiek van de waarde van de coins druk op G.
+    Om het spel te spelen type Play.
+    Als je klaar bent kan je met programma stoppen met quit(Q).
+    """
 
-    Alice = Investor(money=start_money, coin_data=coins_data['ALB'])
-    Alice.invest(Investor.buy_at_rate, Investor.sell_at_rate, buy_rate=1500, sell_rate=1600)
-    print(Alice.money)
+    print("[T] Table with statistics")
+    print("[I] Outcome for the 6 investers")
+    print("[G] The Graph")
+    print("[play] To play The game")
+    print("[Q] Quit program")
+
+    coins_data = parse_json()
+    choice = input("choose:")
+    run = True
+
+    while run is True:
+        if choice in ("t", "T"):
+            draw_table(coins_data)
+            break
+        if choice in ("i", "I"):
+            pass
+        if choice in ("g", "G"):
+            pass
+        if choice in ("play", "Play"):
+            pass
+        if choice in ("q", "Q"):
+            run = False
+
+
+if __name__ == "__main__":
+    main()
